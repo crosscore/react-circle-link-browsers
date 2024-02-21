2722486618c9331c67edbfd1b82c14338564abdd
yuu <crosscore.g@gmail.com>
2024-02-21 19:24:21 +0900

    update player.js

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -1,11 +1,11 @@
 // react-circle-link-browsers/websocket-server/index.js
 const WebSocket = require("ws");
 const {
-  createCircle,
-  updateCircles,
-  sendCirclePositions,
-  removeOldCircles,
-  switchPattern,
+	createCircle,
+	updateCircles,
+	sendCirclePositions,
+	removeOldCircles,
+	switchPattern,
 } = require("./circleMotion");
 const Player = require("./player");
 
@@ -15,43 +15,51 @@ const isOpen = (ws) => ws.readyState === WebSocket.OPEN;
 const player = new Player();
 
 if (process.env.NODE_ENV === "development") {
-  console.log("websocket server running on port 8080");
+	console.log("websocket server running on port 8080");
 }
 
 function updateCirclePosition() {
-  updateCircles();
-  sendCirclePositions(wss, clientWindowInfo, isOpen);
+	updateCircles();
+	sendCirclePositions(wss, clientWindowInfo, isOpen);
 }
 
+function updatePlayerPosition() {
+	player.update();
+}
+
+setInterval(updatePlayerPosition, 9);
 setInterval(createCircle, 500);
 setInterval(updateCirclePosition, 9);
 setInterval(removeOldCircles, 1000);
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    const msg = JSON.parse(message);
-    if (msg.type === 'windowInfo') {
-      clientWindowInfo.set(ws, msg.data);
-    }
-  });
+wss.on("connection", (ws) => {
+	ws.on("message", (message) => {
+		const msg = JSON.parse(message);
+		if (msg.type === "windowInfo") {
+			clientWindowInfo.set(ws, msg.data);
+		}
+	});
 
-  ws.on('message', message => {
-    const msg = JSON.parse(message);
-    if (msg.type === 'switchPattern') {
-      switchPattern();
-    }
-  });
+	ws.on("message", (message) => {
+		const msg = JSON.parse(message);
+		if (msg.type === "switchPattern") {
+			switchPattern();
+		}
+	});
 
-  wss.on('connection', ws => {
-    ws.on('message', message => {
-      const msg = JSON.parse(message);
-      if (msg.type === 'movePlayer') {
-        player.move(msg.direction);
-      };
-    });
+	wss.on("connection", (ws) => {
+		ws.on("message", (message) => {
+			const msg = JSON.parse(message);
+			if (msg.type === "windowInfo") {
+				clientWindowInfo.set(ws, msg.data);
+				const initialX = msg.data.screenX + msg.data.innerWidth / 2;
+				const initialY = msg.data.screenY + msg.data.innerHeight / 2;
+				player = new Player(initialX, initialY);
+			}
+		});
 
-    ws.on('close', () => {
-      clientWindowInfo.delete(ws);
-    });
-  });
+		ws.on("close", () => {
+			clientWindowInfo.delete(ws);
+		});
+	});
 });
--- websocket-server/player.js
+++ websocket-server/player.js
@@ -1,37 +1,36 @@
 // react-circle-link-browsers/websocket-server/player.js
 class Player {
-  constructor() {
-    this.position = { x: 0, y: 0 }; // initial position
-    this.velocity = { x: 0, y: 0 }; // initial velocity
-  }
+	constructor(initialX, initialY) {
+		this.position = { x: initialX, y: initialY }; // Updated initial position
+		this.velocity = { x: 0, y: 0 };
+	}
 
-  move(direction) {
+	move(direction) {
+		const speed = 5;
+		switch (direction) {
+			case "w": // up
+				this.velocity.y = -speed;
+				break;
+			case "a": // left
+				this.velocity.x = -speed;
+				break;
+			case "s": // down
+				this.velocity.y = speed;
+				break;
+			case "d": // right
+				this.velocity.x = speed;
+				break;
+			default: // stop
+				this.velocity.x = 0;
+				this.velocity.y = 0;
+		}
+	}
 
-    const speed = 5;
-    switch (direction) {
-      case 'w': // up
-        this.velocity.y = -speed;
-        break;
-      case 'a': // left
-        this.velocity.x = -speed;
-        break;
-      case 's': // down
-        this.velocity.y = speed;
-        break;
-      case 'd': // right
-        this.velocity.x = speed;
-        break;
-      default: // stop
-        this.velocity.x = 0;
-        this.velocity.y = 0;
-    }
-  }
-
-  update() {
-    // update the position based on the velocity
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  }
+	update() {
+		// update the position based on the velocity
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+	}
 }
 
 module.exports = Player;
